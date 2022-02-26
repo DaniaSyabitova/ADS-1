61ccd2d6d3712f22de046899f3b3bba91b3c04fc
Dania Syabitova <94595600+DaniaSyabitova@users.noreply.github.com>
2022-02-26 19:38:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
   bool res=true;
-  for (uint64_t i=2; i<=(value/2); ++i){
+  for (uint64_t i=2; i <= (value/2); ++i){
     if (value%i == 0)
       res=false;
   }
@@ -18,16 +18,31 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   while (1) {
-    int c=1;
+    int n=1;
     value++;
-    for (int i=2; i<=value; i++)
+    for (int i=2; i <= value; i++)
       if (value%i == 0)
-        c++;
-      if (c == 2)
+        n++;
+      if (n == 2)
         return value;
   }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int summa = 0;
+  int *a = new int[hbound+1];
+  for (int i=0; i <= hbound; ++i) 
+    a[i] = i;
+  for (int i=2; i*i <= hbound; i++) {
+    if (a[i]) {
+      for (int j=i*i; j <= hbound; j+=i)
+        a[j]=0;
+    }
+  }
+  for (int i=2; i < hbound; ++i) {
+    if (a[i]) {
+      summa = summa + a[i]:
+    }
+  }
+  return summa;
 }
